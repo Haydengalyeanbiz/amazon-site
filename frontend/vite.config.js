1a16a121554cd5d2c49d6ef687b4254d65514a67
Hayden <haydengalyeanbiz@gmail.com>
2024-12-20 00:33:21 -0500

    added proxy to the frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig((mode) => ({
+	plugins: [
+		vue(),
+		eslintPlugin({
+			lintOnStart: true,
+			failOnError: mode === 'production',
+		}),
+	],
+	server: {
+		open: true,
+		proxy: {
+			'/api': 'http://127.0.0.1:8000',
+		},
+	},
+}));
