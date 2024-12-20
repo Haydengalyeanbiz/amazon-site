25b8e8ada63657b81284f6ca0d67c5c0a2f42341
Hayden <haydengalyeanbiz@gmail.com>
2024-12-20 14:06:01 -0500

    added request functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, redirect
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_cors import CORS
 from flask_login import LoginManager
 from .models import db, User
@@ -7,6 +8,7 @@ from .config import Config
 from .seeders import seed_commands
 from .api.auth_routes import auth_routes
 from .api.post_routes import post_routes
+import os
 
 app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
 
@@ -27,3 +29,53 @@ app.register_blueprint(post_routes, url_prefix='/api/posts')
 
 Migrate(app, db)
 CORS(app)
+
+@app.before_request
+def https_redirect():
+    if os.environ.get('FLASK_ENV') == 'production':
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
+
+
+@app.after_request
+def inject_csrf_token(response):
+    response.set_cookie(
+        'csrf_token',
+        generate_csrf(),
+        secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
+        samesite='Strict' if os.environ.get(
+            'FLASK_ENV') == 'production' else None,
+        httponly=True)
+    return response
+
+
+@app.route("/api/docs")
+def api_help():
+    """
+    Returns all API routes and their doc strings
+    """
+    acceptable_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    route_list = { rule.rule: [[ method for method in rule.methods if method in acceptable_methods ],
+                    app.view_functions[rule.endpoint].__doc__ ]
+                    for rule in app.url_map.iter_rules() if rule.endpoint != 'static' }
+    return route_list
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    """
+    This route will direct to the public directory in our
+    react builds in the production environment for favicon
+    or index.html requests
+    """
+    if path == 'favicon.ico':
+        return app.send_from_directory('public', 'favicon.ico')
+    return app.send_static_file('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
