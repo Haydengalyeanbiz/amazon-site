e27d75f6c648da060409aad42406d2e9f7688a77
Hayden <haydengalyeanbiz@gmail.com>
2024-12-24 00:49:36 -0500

    added length validators to the postform

--- app/forms/PostForm.py
+++ app/forms/PostForm.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Optional
+from wtforms.validators import DataRequired, Optional, Length
 
 
 class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    price = StringField('Price', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
-    image_url = StringField('Image URL', validators=[DataRequired()])
-    link_url = StringField('Link URL', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired(), Length(max=250)])
+    price = StringField('Price', validators=[DataRequired(), Length(max=100)])
+    description = StringField('Description', validators=[DataRequired(), Length(max=1000)])
+    image_url = StringField('Image URL', validators=[DataRequired(), Length(max=300)])
+    link_url = StringField('Link URL', validators=[DataRequired(), Length(max=1000)])
     submit = SubmitField('Post')
